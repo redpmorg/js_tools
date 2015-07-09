b61287a8197cf146bf9c11485d6163409ef8f527
redgoat.org <leonardlepadatu@yahoo.com>
2015-07-09 18:02:15 +0300

    save work

--- validation_rules.js
+++ validation_rules.js
@@ -5,10 +5,10 @@
  * @version $Id$
  */
 
- var validationMessageLanguage = 'ro';
+ // var validationMessageLanguage = '';
 
  if (!validationMessageLanguage) {
- 	var validationMessageLanguage = "en";
+	var validationMessageLanguage = "fr";
  }
 
  var validationParameters = {
@@ -46,6 +46,31 @@
 				en: "Please enter a valid email!",
 				fr: "S'il vous plaît entrer une adresse email valide!",
 				ro: "Va rog introduceti o adresa de email valida!"
+			},
+			validDomain: function(d) {
+				return {
+					en: "Please enter a valid email on domain " + d +" !",
+					fr: "S'il vous plaît entrer une adresse email valide on domaine " + d +" !",
+					ro: "Va rog introduceti o adresa de email valida in domeniul " + d +" !"
+				}
+			}
+		},
+		date: {
+			valid: {
+				en: "Please enter a valid email!",
+				fr: "S'il vous plaît entrer une adresse email valide!",
+				ro: "Va rog introduceti o adresa de email valida!"
+			},
+
+AICI AM RAMAS
+
+
+			greaterThan: function(d) {
+				return {
+					en: "Please enter a valid email on domain " + d +" !",
+					fr: "S'il vous plaît entrer une adresse email valide on domaine " + d +" !",
+					ro: "Va rog introduceti o adresa de email valida in domeniul " + d +" !"
+				}
 			}
 		}
 	},
@@ -84,13 +109,22 @@
 
 		email:{
 			valid: function(v) {
-				var myRegExp = /^[A-Z0-9._%+-]+@(([A-Z0-9.-]+\.[A-Z])|groupehn.com)$/i;
+				var myRegExp = /^[A-Z0-9._%\+-]+@[A-Z0-9.-]+\.[a-z]{2,4}$/i;
 				var valid = (myRegExp.test(v)) ? true : false;
 				return {
 					validity: valid,
 					message: validationParameters.messages.email.valid[validationMessageLanguage]
 				}
 			},
+			validDomain: function(v) {
+				var validEmailDomain = "groupehn.com";
+				var myRegExp = new RegExp("/^[a-zA-Z0-9._%\+-]+" + validEmailDomain + "$/", "i");
+				var valid = (myRegExp.test(v)) ? true : false;
+				return {
+					validity: valid,
+					message: validationParameters.messages.email.validDomain(validEmailDomain)[validationMessageLanguage]
+				}
+			}
 		},
 
 		color: {
@@ -103,20 +137,63 @@
 				}
 			}
 		},
-	}
+
+		date: {
+			valid: function(v) {
+				var valid = ("" != v) ? true : false;
+				return {
+					validity: valid,
+					message: validationParameters.messages.date.valid[validationMessageLanguage]
+				}
+			},
+			greaterThan: function(v, param) {
+				var refDate = $('[name="'+ param +'"]').val();
+				var myDate = new Date(v);
+				refDate = new Date(refDate);
+				if(("Invalid Date" !== myDate && "Invalid Date" === refDate) ||
+					(("Invalid Date" !== myDate && "Invalid Date" !== refDate) && (myDate > refDate))) {
+					valid = true;
+				} else {
+					valid = false;
+				}
+				return {
+					validity: valid,
+					message: validationParameters.messages.date.greaterThan(param)[validationMessageLanguage]
+				}
+			}
+		},
+	},
  }
 
- $("#submit-button").on("click", function() {
+ $(".submit-button").on("click", function() {
+ 	$(".validationMessages").prev("input, select, texarea").removeClass("input-error");
 	$(".validationMessages").remove();
 	var form = $(this).closest('form');
 	var inp = $("input, select, texarea", form).each(function(){
 		$this = $(this);
 		if($this.data() && $this.data("req")) {
 			var validationRules = $this.data("req");
+
+			// rK - ruleKey, rV - ruleValue, cR - complexRule, cK - complexKey, cV - complexValue
 			for(ruleId in validationRules){
-				var rule = validationParameters.rules[ruleId][validationRules[ruleId]];
-				rule = rule($this.val());
+				var param = null,
+					rK = ruleId,
+				    rV = validationRules[rK],
+				    ruleObj = validationParameters.rules,
+				    rule = ruleObj[rK][rV];
+
+				// complex rule with parameter. One level nested.
+				// For more use param as array.
+				if ("object" == typeof rV) {
+					for(cK in rV) {
+						rule = ruleObj[rK][cK];
+					    param = rV[cK];
+					}
+				}
+				rule = rule($this.val(), param);
+//				console.log(rule);   // Debug Rule
 				if(!rule.validity) {
+					$this.addClass("input-error");
 					$this.after("<p class='validationMessages text-danger'>" +rule.message+ "</p>");
 				}
 			}
